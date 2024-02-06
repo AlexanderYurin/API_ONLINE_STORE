b465e4d9bf6166e3bbf63bfdb5cd93948431a30b
Alexander Yurin <hiimmandarin32@gmail.com>
2024-02-06 20:15:43 +0300

    довавлен swager

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,14 +18,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
 
 from app import settings
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Store API",
+        default_version="v1",
+        description="Итоговый проект",
+        terms_of_service="http://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="admin@company.local"),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("catalog.urls")),
     path("api/profile/", include("users.urls", namespace="user")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
 ]
 
 if settings.DEBUG:
